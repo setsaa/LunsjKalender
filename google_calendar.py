de4bf982116ef08d5c02eb802e441404474856d2
setsaa <sigurd@setsa.no>
2025-01-29 10:26:36 +0100

    fixed bug

--- google_calendar.py
+++ google_calendar.py
@@ -5,9 +5,6 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Google Calendar API scope
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
